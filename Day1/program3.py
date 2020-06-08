4416bd87d5037cdce612fd46e788eda91551a0d8
Mahak Gupta <64647674+mahak2106@users.noreply.github.com>
2020-06-08 15:58:04 +0530

    Create program3.py

---
+++ Day1/program3.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+
+a=a+b
+b=a-b
+a=a-b
+
+print("Afer Swapping")
+print("The value of a is ",a)
+print("The value of b is ",b)
