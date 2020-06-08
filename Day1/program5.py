a6f80135e3b393ede4484c59a085f8c28f0a767f
Mahak Gupta <64647674+mahak2106@users.noreply.github.com>
2020-06-08 15:59:15 +0530

    Create program5.py

---
+++ Day1/program5.py
@@ -0,0 +1,17 @@
+p1=int(input("Enter the runs scored by player 1 :"))
+p2=int(input("Enter the runs scored by player 2 :"))
+p3=int(input("Enter the runs scored by player 3 :"))
+
+print("Strike Rate of Player 1 is ",(p1/60)*100)
+print("Strike Rate of Player 2 is ",(p2/60)*100)
+print("Strike Rate of Player 3 is ",(p3/60)*100)
+
+
+print("If it plays more 60 balls then it score ",2*p1)
+print("If it plays more 60 balls then it score ",2*p2)
+print("If it plays more 60 balls then it score ",2*p3)
+
+
+print("MAximum number of sixes can player hit ",p1//6)
+print("MAximum number of sixes can player hit ",p2//6)
+print("MAximum number of sixes can player hit ",p3//6)
